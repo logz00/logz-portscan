672e6a03f226e037b45271afe1139058ad3082e2
Logz00 <logan.smith.tech@gmail.com>
2026-08-05 09:43:53 -0400

    updates

--- main.py
+++ main.py
@@ -3,45 +3,45 @@ import os
 
 
 def host_enum():
-    subnet = input("Please enter target subnet range (ex. 192.168.1.1/24) >> ").strip()
-    choice = input("Would you like to grep the results to only see the hosts IPs (y/n) ").strip().lower()
+    subnet = input("\nPlease enter target subnet range (ex. 192.168.1.1/24) >> ").strip()
+    choice = input("Would you like to grep the results to only see the hosts IPs (y/n): ").strip().lower()
     if choice == "y":
         os.system("clear")
-        print(f"\n[>] Starting Host Enumeration on {subnet}...")
+        print(f"\n[>] Starting Host Enumeration on {subnet}...\n")
         os.system(f"sudo nmap -sn {subnet} | grep 'for'")
     elif choice == "n":
         os.system("clear")
-        print(f"\n[>] Starting Host Enumeration on {subnet}...")
+        print(f"\n[>] Starting Host Enumeration on {subnet}...\n")
         os.system(f"sudo nmap -sn {subnet}")
     else:
         print("[!] Invalid input, expected y or n")
 
 
 def service_enum():
-    target = input("Target Device IP >> ").strip()
-    choice = input("Are you checking a specific port? (y/n) ").strip().lower()
+    target = input("\nTarget Device IP >> ").strip()
+    choice = input("Are you checking a specific port? (y/n): ").strip().lower()
     if choice == "y":
         port = input("Target Port >> ").strip()
         os.system("clear")
-        print("\n[>] Starting Service Enumeration...")
+        print("\n[>] Starting Service Enumeration...\n")
         os.system(f"sudo nmap -sV -sC -O -p {port} {target}")
     elif choice == "n":
         os.system("clear")
-        print("\n[>] Starting Service Enumeration...")
+        print("\n[>] Starting Service Enumeration...\n")
         os.system(f"sudo nmap -sV -sC -O {target}")
     else:
         print("[!] Invalid input, expected y or n")
 
 
 def port_enum():
-    target = input("Target Device IP >> ").strip()
+    target = input("\nTarget Device IP >> ").strip()
     os.system("clear")
-    print("\n[>] Starting TCP Scan...")
+    print("\n[>] Starting TCP Scan...\n")
     os.system(f"sudo nmap -sS -Pn {target}")
 
 
 def exit_program():
-    print("[!] Exiting the program....\n")
+    print("\n[!] Exiting the program....\n")
     time.sleep(2)
 
 # program loop
@@ -50,6 +50,7 @@ while True:
     print(">> Python Port Scanner using Nmap by Logz <<")
     print("")
     print("[>] Scan Options ")
+    print("")
     print("[1] LAN Host Enumeration")
     print("[2] TCP Port Scan (open ports)")
     print("[3] Service Enumeration")
